f96cd2e8e7b85ffeb1c45171f94b227d67c97113
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-05-28 13:00:23 +0530

    fix some minor charges

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice";
 import { logoutUser } from "../../services/authService";
 
-
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -22,7 +21,9 @@ const Header = () => {
       <div className="--flex-between">
         <h3>
           <span className="--fw-thin">Welcome, </span>
-          <span className="--color-danger">{name}</span>
+          <span className="--color-danger">
+            {name[0].toUpperCase() + name.slice(1)}
+          </span>
         </h3>
         <button onClick={logout} className="btnLogout">
           Logout
